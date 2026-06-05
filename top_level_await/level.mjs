0a26ffa2072e364258da98dc546a74b616229ade
Progresso2001 <joaquimmenianga99@gmail.com>
2026-06-05 13:24:16 +0530

    atualizando dados

--- top_level_await/level.mjs
+++ top_level_await/level.mjs
@@ -1,91 +1,97 @@
-const usuarios = await fetch("https://jsonplaceholder.typicode.com/users/1")
+// const usuarios = await fetch("https://jsonplaceholder.typicode.com/users/1")
 
+// const res = usuarios
+
+// export { res }
+
+// // Carregamento Dinâmico de Módules com Fallback
+// // com suporte CommonJS module
+// let module,
+// try{
+//     const moduloPrincipal = await import("https://cdn1.com/library.js")
+//     console.log("Carregado CDN Principal")
+// }
+// catch{
+//     const moduleSecundario = await import("https://cdn2.com/library.js")
+//     console.log("Carregado CDN Secundario")
+// }
+
+// export const formatDate = moduloPrincipal.formatDate
+// export const parseDate = moduloSecundario.parseDate
+
+
+// let usuarios;
+// export const fetchDados = async()=>{
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users')
+//     usuarios = res.json()
+//     return usuarios
+// } 
+// fetchDados()
+// export { usuarios }
+
+// // **************************************Investigando Funces IIFE**********************************
+// const obterDados = async() =>{
+//     const url = "https://jsonplaceholder.typicode.com/users/1"
+//     const res = await fetch(url)
+//     return res.json()
+// }
+// obterDados()
+//     .then(res=>res)
+//     .then(data=>console.log(data))
+//     .catch(e=>console.error("ERRO:", e))
+
+// // Passar parametro
+
+// const obterDados = async(id) =>{
+//     const url = `https://jsonplaceholder.typicode.com/users/${id}`
+//     const res = await fetch(url)
+//     return res.json()
+// }
+// obterDados(1)
+//     .then(res=>res)
+//     .then(data=>console.log(data))
+//     .catch(e=>console.error("ERRO:", e))
+
+// //  Função Anônima (expressão async function atribuída a variável)
+// // Expressão async function atribuída a variável (função anônima nomeada pela variável)
+
+// const buscarDados = async function(userId) {
+//   try {
+//     const resposta = await axios.get(`https://api.exemplo.com/users/${userId}`);
+//     return resposta.data;
+//   } catch (erro) {
+//     console.error('Erro:', erro);
+//     throw erro;
+//   }
+// };
+
+// // OU com arrow function async (mais comum hoje)
+// const buscarDados = async (userId) => {
+//   const resposta = await axios.get(`https://api.exemplo.com/users/${userId}`);
+//   return resposta.data;
+// };
+
+// // Função Anônima IIFE (Invocada Imediatamente)
+
+// // IIFE async - executa imediatamente
+// (async function() {
+//   try {
+//     const dados = await axios.get('https://api.exemplo.com/data');
+//     console.log(dados.data);
+//   } catch (erro) {
+//     console.error(erro);
+//   }
+// })();
+
+// // OU com arrow function
+// (async () => {
+//   const dados = await axios.get('https://api.exemplo.com/data');
+//   console.log(dados.data);
+// })();
+
+
+
+const usuarios = await fetch("https://jsonplaceholder.typicode.com/users/1")
 const res = usuarios
 
 export { res }
-
-// Carregamento Dinâmico de Módules com Fallback
-// com suporte CommonJS module
-let module,
-try{
-    const moduloPrincipal = await import("https://cdn1.com/library.js")
-    console.log("Carregado CDN Principal")
-}
-catch{
-    const moduleSecundario = await import("https://cdn2.com/library.js")
-    console.log("Carregado CDN Secundario")
-}
-
-export const formatDate = moduloPrincipal.formatDate
-export const parseDate = moduloSecundario.parseDate
-
-
-let usuarios;
-export const fetchDados = async()=>{
-    const res = await fetch('https://jsonplaceholder.typicode.com/users')
-    usuarios = res.json()
-    return usuarios
-} 
-fetchDados()
-export { usuarios }
-
-// **************************************Investigando Funces IIFE**********************************
-const obterDados = async() =>{
-    const url = "https://jsonplaceholder.typicode.com/users/1"
-    const res = await fetch(url)
-    return res.json()
-}
-obterDados()
-    .then(res=>res)
-    .then(data=>console.log(data))
-    .catch(e=>console.error("ERRO:", e))
-
-// Passar parametro
-
-const obterDados = async(id) =>{
-    const url = `https://jsonplaceholder.typicode.com/users/${id}`
-    const res = await fetch(url)
-    return res.json()
-}
-obterDados(1)
-    .then(res=>res)
-    .then(data=>console.log(data))
-    .catch(e=>console.error("ERRO:", e))
-
-//  Função Anônima (expressão async function atribuída a variável)
-// Expressão async function atribuída a variável (função anônima nomeada pela variável)
-
-const buscarDados = async function(userId) {
-  try {
-    const resposta = await axios.get(`https://api.exemplo.com/users/${userId}`);
-    return resposta.data;
-  } catch (erro) {
-    console.error('Erro:', erro);
-    throw erro;
-  }
-};
-
-// OU com arrow function async (mais comum hoje)
-const buscarDados = async (userId) => {
-  const resposta = await axios.get(`https://api.exemplo.com/users/${userId}`);
-  return resposta.data;
-};
-
-// Função Anônima IIFE (Invocada Imediatamente)
-
-// IIFE async - executa imediatamente
-(async function() {
-  try {
-    const dados = await axios.get('https://api.exemplo.com/data');
-    console.log(dados.data);
-  } catch (erro) {
-    console.error(erro);
-  }
-})();
-
-// OU com arrow function
-(async () => {
-  const dados = await axios.get('https://api.exemplo.com/data');
-  console.log(dados.data);
-})();
-
--- top_level_await/top.mjs
+++ top_level_await/top.mjs
@@ -1,27 +1,43 @@
-// import { res  } from "./level.js";
-// const user = res;
-// const meuUser = user.json()
-// .then(res=>res)
-// .then(data=>console.log("Dado acessado: ",data))
-// .catch(e=>console.error("Erro:", e.message))
+import { res  } from "./level.js";
+const user = res;
+const meuUser = user.json()
+.then(res=>res)
+.then(data=>console.log("Dado acessado: ",data))
+.catch(e=>console.error("Erro:", e.message))
 
-// // Usando o bloco try e catch
-// try{
-//     const userNovo = await user.json()
-//     if(res.ok){
-//         console.log("Requisicao feita com sucesso: ",res.status)
-//         const data = userNovo
-//         console.log(data)
-//         console.log("Nome da empresa:",userNovo.company?.name)
-//     }
-// }
-// catch(e){
-//     console.error("Erro: ", e.message)
-// }
+// Usando o bloco try e catch
+try{
+    const userNovo = await user.json()
+    if(res.ok){
+        console.log("Requisicao feita com sucesso: ",res.status)
+        const data = userNovo
+        console.log(data)
+        console.log("Nome da empresa:",userNovo.company?.name)
+    }
+}
+catch(e){
+    console.error("Erro: ", e.message)
+}
 
 
-import { usuarios } from "./level.mjs";
+// import { usuarios } from "./level.mjs";
 
-const saidaUser = await usuarios
-console.log("User: ",saidaUser)
+// const saidaUser = await usuarios
+// console.log("User: ",saidaUser)
+
+import { res } from "./level.mjs"
+
+const dadoUser =  res
+try{
+    const userNovo = await dadoUser.json()
+    if(res.ok){
+        console.log("Request boa:",res.status)
+        const data = userNovo
+        console.log(data)
+        
+    }
+}
+catch(e){
+    console.error("Erro: ", e.message)
+}
 
