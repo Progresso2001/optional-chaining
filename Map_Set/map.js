4ba4d34ddb3e611d6f2e105b475019e92eaa05b5
Progresso2001 <joaquimmenianga99@gmail.com>
2026-06-08 18:53:23 +0530

    exemplos comuns de usos

--- Map_Set/map.js
+++ Map_Set/map.js
@@ -1,21 +1,21 @@
-// const mapa = new Map()
-// mapa.set("nome", "DevJocas")
-// mapa.set('id:1', {nome:"DevJocas", idade:22})
-// mapa.set(45, "Javascript")
-// console.log(mapa)
+const mapa = new Map()
+mapa.set("nome", "DevJocas")
+mapa.set('id:1', {nome:"DevJocas", idade:22})
+mapa.set(45, "Javascript")
+console.log(mapa)
 
 
 
 
-// console.log(mapa.has(45))
-// if(mapa.has(45)){
-//     console.log("Contem:", mapa.get(45))
-// }
+console.log(mapa.has(45))
+if(mapa.has(45)){
+    console.log("Contem:", mapa.get(45))
+}
 
-// console.log(mapa.has('id:1'))
-// if(mapa.has('id:1')){
-//     console.log("Dados:", JSON.stringify(mapa.get('id:1')))
-// }
+console.log(mapa.has('id:1'))
+if(mapa.has('id:1')){
+    console.log("Dados:", JSON.stringify(mapa.get('id:1')))
+}
 
 // usando o iteracao
 const usuario = new Map()
@@ -25,5 +25,17 @@ usuario.set("profisao","Analista de dados")
 // console.log(usuario)
 
 for(const [chave, valor] of usuario.entries()){
+    console.log(`${chave}: ${valor}`)
+}
+
+// criando Map usando Array
+
+const map = new Map([
+  ['firstName', 'Luke'],
+  ['lastName', 'Skywalker'],
+  ['occupation', 'Jedi Knight'],
+])
+
+for(let [chave, valor] of map){
     console.log(`${chave}: ${valor}`)
 }
